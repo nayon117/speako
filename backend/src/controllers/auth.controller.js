656277ca3f6f2c5be28bab2baa9a2b338f7ca7fd
nayon117 <hasibul.nayon1@gmail.com>
2024-11-25 16:39:00 +0600

    create message model

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async(req, res) => {
   const {fullName,email,password} = req.body;
@@ -43,8 +44,7 @@ export const signup = async(req, res) => {
   }
 };
 
-
-
+// login
 export const login = async(req, res) => {
   const {email,password} = req.body;
   try {
@@ -79,5 +79,29 @@ export const logout = (req, res) => {
 };
 
 export const updateProfile = async(req, res) => {
-  
+  try {
+    const {profilePic} = req.body;
+    const userId = req.user._id;
+
+    if(!profilePic) return res.status(400).json({message: "Profile pic is required"});
+
+   const uploadResponse =  await cloudinary.uploader.upload(profilePic);
+
+   const updatedUser = await User.findByIdAndUpdate(userId,{profilePic: uploadResponse.secure_url},{new: true});
+
+   return res.status(200).json(updatedUser);
+
+  } catch (error) {
+    console.error("Error in updateProfile controller",error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
+
+export const checkAuth = async(req, res) => {
+  try {
+    return res.status(200).json(req.user);
+  } catch (error) {
+    console.error("Error in checkAuth controller",error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, logout, signup, updateProfile } from '../controllers/auth.controller.js';
+import { checkAuth, login, logout, signup, updateProfile } from '../controllers/auth.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 const router = express.Router();
 
@@ -7,5 +7,7 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout',logout);
 router.put('/update-profile',protectRoute,updateProfile);
+
+router.get('/check',protectRoute,checkAuth);
 
 export default router;
