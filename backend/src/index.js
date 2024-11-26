11fe713c62aafcbe6968fc2f03bcf38b9b8bf91c
nayon117 <hasibul.nayon1@gmail.com>
2024-11-26 22:11:33 +0600

    add file limit

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const __dirname = path.resolve();
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors({
   origin: 'http://localhost:5173',
